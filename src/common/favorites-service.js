880c4c1ceb50f89eab1e118f3b2f11247c8bca9a
Anton Komarnytskyi <anton0886@outlook.com>
2025-12-11 08:39:41 +0200

    refactor: Update FavoritesService to use imdbID for film identification

--- src/common/favorites-service.js
+++ src/common/favorites-service.js
@@ -1,13 +1,17 @@
 export class FavoritesService {
   static add(appState, film) {
-    const exists = appState.favorites.find(({ id }) => id === film.id);
+    const exists = appState.favorites.find(
+      ({ imdbID }) => imdbID === film.imdbID
+    );
     if (!exists) {
       appState.favorites = [...appState.favorites, film];
     }
   }
 
   static remove(appState, film) {
-    appState.favorites = appState.favorites.filter(({ id }) => id !== film.id);
+    appState.favorites = appState.favorites.filter(
+      ({ imdbID }) => imdbID !== film.imdbID
+    );
   }
 
   static toggle(appState, film, isFavorite) {
